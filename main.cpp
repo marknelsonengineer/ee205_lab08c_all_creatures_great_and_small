cde49b13c5617de229b45fc22a231d28a9a3e46e
Mark Nelson <marknels@hawaii.edu>
2022-03-03 06:27:36 -1000

    Add Header to main.cpp Removed un-needed printf

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
-//#include <iostream>
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab08c_all_creatures_great_and_small - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// @author Mark Nelson <marknels@hawaii.edu>
+/// @date   02_Mar_2022
+///////////////////////////////////////////////////////////////////////////////
 
 #include "Animal.h"
 
 int main() {
-   // std::cout << "Welcome to All Creatures Great and Small!" << std::endl;
-
    Animal myFirstAnimal = Animal( "Felis catus" );
    myFirstAnimal.setGender( MALE );
    myFirstAnimal.setWeight( 19 );  // @todo Refactor to include Unit of Measure
